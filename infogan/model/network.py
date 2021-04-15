66f3bc97483117ab18dc484e2d8f52853ca24661
Jihoon Kim <jihoonerd@outlook.com>
2021-04-15 10:47:32 +0900

    Put infogan q network into discriminator

--- infogan/model/network.py
+++ infogan/model/network.py
@@ -1,11 +1,8 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.nn.modules.linear import Linear
 
 
 class Generator(nn.Module):
-    def __init__(self, noise_dim=32, continuous_code_dim=1, discrete_code_dim=0,  out_dim=2):
+    def __init__(self, noise_dim, discrete_code_dim, continuous_code_dim, out_dim):
         super().__init__()
 
         self.fc = nn.Sequential(
@@ -22,27 +19,42 @@ class Generator(nn.Module):
 
 
 class Discriminator(nn.Module):
-    def __init__(self, data_dim=2):
+    def __init__(self, data_dim, discrete_code_dim, continuous_code_dim):
         super().__init__()
 
-        self.fc = nn.Sequential(
-            nn.Linear(data_dim, 32),
+        self.data_dim = data_dim
+        self.discrete_code_dim = discrete_code_dim
+        self.continuous_code_dim = continuous_code_dim
+
+        self.discriminator = nn.Sequential(
+            nn.Linear(data_dim, 64),
             nn.LeakyReLU(0.1),
+            nn.Linear(64, 32)
+        )
+
+        self.regular_gan = nn.Sequential(
             nn.Linear(32, 1)
         )
 
-    def forward(self, x):
-        out = self.fc(x)
-        out = torch.sigmoid(out)
-        return out
+        self.infogan_q = nn.Sequential(
+            nn.Linear(32, 16),
+            nn.LeakyReLU(0.1),
+            nn.Linear(16, discrete_code_dim + continuous_code_dim * 2)  # continuous code needs to have mu, sigma respectively
+        )
 
-
-class QNetwork(nn.Module):
-    def __init__(self, data_dim=2, continuous_code_dim=1, discrete_code_dim=0):
-        super().__init__()
-
-        self.fc = nn.Linear(data_dim, continuous_code_dim + discrete_code_dim)
+        self.sigmoid = nn.Sigmoid()
+        self.softplus = nn.Softplus()
 
     def forward(self, x):
-        out = self.fc(x)
-        return out
+        discriminator_out = self.discriminator(x)
+        regular_gan_out = self.sigmoid(self.regular_gan(discriminator_out))
+        
+        q_out = self.infogan_q(discriminator_out)
+        
+        q_discrete = self.softplus(q_out[:,:self.discrete_code_dim])
+        
+        q_continuous = q_out[:,self.discrete_code_dim:]  # e.g.) [cont_mu1, cont_mu2, cont_sigma1, cont_simga2]
+        q_mu = q_continuous[:, :self.continuous_code_dim]
+        q_sigma = self.softplus(q_continuous[:, self.continuous_code_dim:])  # Use softplus to make std positive value
+
+        return regular_gan_out, q_discrete, q_mu, q_sigma
--- tests/test_network.py
+++ tests/test_network.py
@@ -3,14 +3,22 @@ from infogan.data.generate_toy_example import generate_circle_toy_data
 import torch
 
 def test_generator_output():
-    data = torch.randn((400, 33))  # noise: 32, code: 1
-    generator = Generator(noise_dim=32, continuous_code_dim=1, discrete_code_dim=0, out_dim=2)
+
+    noise = torch.randn((400, 32))  # noise: 32
+    bernoulli = torch.distributions.bernoulli.Bernoulli(0.5)
+    discrete_code = bernoulli.sample((400, 1))  # discrete code: 1
+    data = torch.cat([noise, discrete_code], dim=1)
+
+    generator = Generator(noise_dim=32, discrete_code_dim=1, continuous_code_dim=0, out_dim=2)
     out = generator(data)
     assert out.shape == (400, 2)
 
 def test_discriminator_output():
     data = torch.Tensor(generate_circle_toy_data())
-    discriminator = Discriminator(data_dim=2, continuous_code_dim=1, discrete_code_dim=0)
-    out = discriminator(data)
-    assert out.shape == (400, 1)
-    
+    discriminator = Discriminator(data_dim=2, discrete_code_dim=3, continuous_code_dim=2)
+    regular_gan_out, q_discrete, q_mu, q_sigma = discriminator(data)
+
+    assert regular_gan_out.shape == (400, 1)
+    assert q_discrete.shape[1] == 3
+    assert q_mu.shape[1] == 2
+    assert q_sigma.shape[1] == 2

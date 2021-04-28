e98346c099582492ee929b2d4b3b7783bbdd8f36
Jihoon Kim <jihoonerd@outlook.com>
2021-04-28 20:03:44 +0900

    WIP: exp4

--- infogan/data/generate_toy_example.py
+++ infogan/data/generate_toy_example.py
@@ -21,27 +21,17 @@ def generate_circle_toy_data_by_angle():
     target_theta = 0
 
     if start_theta > target_theta:
-        cw_path = np.linspace(start_theta, target_theta, 18)
-        ccw_path = np.linspace(start_theta, target_theta + 360, 18)
+        cw_path = np.linspace(start_theta, target_theta, 60)
+        ccw_path = np.linspace(start_theta, target_theta + 360, 60)
     else:
-        cw_path = np.linspace(start_theta + 360, target_theta, 18)
-        ccw_path = np.linspace(start_theta, target_theta, 18)
+        cw_path = np.linspace(start_theta + 360, target_theta, 60)
+        ccw_path = np.linspace(start_theta, target_theta, 60)
     
     cw_x = np.array([np.cos(np.radians(degree % 360)) for degree in cw_path]).reshape(-1, 1)
     cw_y = np.array([np.sin(np.radians(degree % 360)) for degree in cw_path]).reshape(-1, 1)
 
     ccw_x = np.array([np.cos(np.radians(degree % 360)) for degree in ccw_path]).reshape(-1, 1)
     ccw_y = np.array([np.sin(np.radians(degree % 360)) for degree in ccw_path]).reshape(-1, 1)
-
-    # common path
-    common_x = np.arange(-1.12, -1.0, 0.04).reshape(-1, 1)
-    common_y = np.zeros_like(common_x)
-
-    cw_x = np.concatenate([common_x, cw_x])
-    cw_y = np.concatenate([common_y, cw_y])
-
-    ccw_x = np.concatenate([common_x, ccw_x])
-    ccw_y = np.concatenate([common_y, ccw_y])
 
     cw_coord = np.concatenate([cw_x, cw_y], axis=1)
     ccw_coord = np.concatenate([ccw_x, ccw_y], axis=1)
